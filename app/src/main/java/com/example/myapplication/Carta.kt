5c19889d54acd0de23a8f777fbaaa44c7dd7beb4
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-29 13:15:26 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/Carta.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication
-
-data class Carta(
-    //Si es true la estamos viendo
-    val front: Int,
-    val back: Int,
-    val state: Boolean
-)
--- app/src/main/java/com/example/myapplication/SegundoEjercicioActivity.kt
+++ app/src/main/java/com/example/myapplication/SegundoEjercicioActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.ImageView
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.ActivitySegundoEjercicioBinding
 
@@ -15,13 +16,6 @@ class SegundoEjercicioActivity : AppCompatActivity() {
 
     private lateinit var countDownTimer: CountDownTimer
     private var tiempoRestante: Long = 2000
-
-    val listaCartas = listOf(
-        Carta(R.drawable.card_2, R.drawable.back_card, false),
-        Carta(R.drawable.card_4, R.drawable.back_card, false),
-        Carta(R.drawable.card_joker, R.drawable.back_card, false)
-    )
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +30,7 @@ class SegundoEjercicioActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                // Acciones a realizar cuando el contador llega a cero
-                binding.ivPrimera.setImageResource(R.drawable.back_card)
-                binding.ivSegunda.setImageResource(R.drawable.back_card)
-                binding.ivTercera.setImageResource(R.drawable.back_card)
-                isPressedButton = false
+                recreate()
             }
         }
 
